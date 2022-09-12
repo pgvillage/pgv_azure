b3824f1393bd1f78c10f4f144062ab0464051253
Sebastiaan Mannem <sebas@mannem.nl>
2022-09-12 11:39:14 +0200

    Rebuilding afresh bootstrap node

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ az vm create \
   --image Debian \
   --admin-username azureuser \
   --generate-ssh-keys \
-  --set tags.applicationRole=ansible_runner
+  --tags applicationRole=ansible_runner
 ```
 * log into it:
 ```
@@ -22,8 +22,20 @@ ssh azureuser@$(az vm show -d -g pgVillage -n pgVControl --query publicIps -o ts
 ```
 curl https://raw.githubusercontent.com/pgvillage/pgv_azure/main/bootstrap_debian.sh | bash
 ```
-* log in to azure: `az login`
-
-## ALternate
+* log in to azure: `az login` or alternatively create a credentials file in ~/.azure/credentials with the required settings (subscription_id, client_id, secret, tenant). Example:
+```
+cat ~/.azure/credentials
+[default]
+subscription_id=abcd1234-98fe-1a2b-f9e8-567cde123abc
+client_id=12ab34cd-56ef-78ab-90cd-1a2b3c4d5e6e
+secret=fK~8Q~anySecretAppliesHere~WhatEverWorks
+tenant=cdef7890-a4b5-5a5b-6b6c-9876543fedcb
+```
+* Run Ansible-playbook:
+```
+cd ~/git/pgv_azure/
+~/.local/bin/ansible-playbook -i environments/cluster1 create_vms.yml
+```
+## Alternate
 * Create a CentOS VM Ansible Control Node on Azure by following [this](https://docs.microsoft.com/en-us/azure/developer/ansible/install-on-linux-vm?tabs=azure-cli#install-ansible-on-an-azure-linux-virtual-machine).
 
--- bootstrap_debian.sh
+++ bootstrap_debian.sh
@@ -4,7 +4,9 @@ set -e
 echo Setup git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 sudo apt-get update -y && sudo apt-get install git -y
-mkdir git && cd git && git clone https://github.com/pgvillage/pgv_azure && cd
+mkdir -p ~/git && cd ~/git && git clone https://github.com/pgvillage/pgv_azure
+mkdir -p ~/git && cd ~/git && git clone https://github.com/pgvillage/pgvillage && cd pgvillage && ln -s ~/git/pgv_azure/environments
+cd
 
 echo Install python3
 # For more info, see https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
@@ -13,10 +15,10 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
 echo Install ansible
 pip3 install --upgrade --user pip ansible azure-cli-core --upgrade cryptography azure-identity
-ansible-galaxy collection install azure.azcollection
+~/.local/bin/ansible-galaxy collection install azure.azcollection
 pip3 install --user -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
 
 echo Setup ssh localhost
-ssh-keygen -q -f ~/.ssh/id_rsa -P ""
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-ssh-keyscan -H localhost >> ~/.ssh/known_hosts
+[ -f ~/.ssh/id_rsa.pub ] || ssh-keygen -q -f ~/.ssh/id_rsa -P ""
+grep -q $USER@$HOSTNAME ~/.ssh/authorized_keys || cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+ssh-keygen -H -F localhost > /dev/null || ssh-keyscan -H localhost >> ~/.ssh/known_hosts
