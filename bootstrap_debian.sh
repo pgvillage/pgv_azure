895192431d304b6f53aad659593dbffc2bfd20c8
Sebastiaan Mannem <sebas@mannem.nl>
2024-06-19 22:40:44 +0200

    minor fixes in bootstrap_debian.sh

--- bootstrap_debian.sh
+++ bootstrap_debian.sh
@@ -2,21 +2,21 @@
 set -e
 
 echo Setup git
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+ssh-keyscan -H github.com >>~/.ssh/known_hosts
 sudo apt-get update -y && sudo apt-get install -y git apt pkg-config libssl-dev
 if [ ! -d ~/git/pgv_azure ]; then
-  mkdir -p ~/git && cd ~/git && git clone https://github.com/pgvillage/pgv_azure
+	mkdir -p ~/git && cd ~/git && git clone https://github.com/pgvillage/pgv_azure
 fi
 if [ ! -d ~/git/pgvillage ]; then
-  mkdir -p ~/git && cd ~/git && git clone https://github.com/pgvillage/pgvillage && cd pgvillage && ln -s ~/git/pgv_azure/environments
+	mkdir -p ~/git && cd ~/git && git clone https://github.com/pgvillage/pgvillage && cd pgvillage && ln -s ~/git/pgv_azure/environments
 fi
 
 echo Install rust
 if ! rustup update; then
-  cd $(MKTEMP -d)
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > sh.rustup.rs
-  bash ./sh.rustup.rs -y
-  export PATH=$HOME/.cargo/bin:$PATH
+	cd $(MKTEMP -d)
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >sh.rustup.rs
+	bash ./sh.rustup.rs -y
+	export PATH=$HOME/.cargo/bin:$PATH
 fi
 
 cd
@@ -27,9 +27,9 @@ sudo apt-get install -y python3-pip libffi-dev
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
 echo Install ansible
-pip3 install --upgrade --user pip ansible azure-cli-core --upgrade cryptography azure-identity packaging netaddr
+pip3 install --upgrade --user pip ansible azure-cli-core --upgrade cryptography azure-identity packaging netaddr msgraph-sdk
 ~/.local/bin/ansible-galaxy collection install azure.azcollection --force
-pip3 install --user -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
+pip3 install --user -r ~/.local/lib/python3.9/site-packages/ansible_collections/azure/azcollection/requirements-azure.txt
 
 echo Install ansible
 pip3 install --upgrade --user chainsmith
@@ -39,7 +39,7 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 echo Setup ssh localhost
 [ -f ~/.ssh/id_rsa.pub ] || ssh-keygen -q -f ~/.ssh/id_rsa -P ""
-grep -q $USER@$HOSTNAME ~/.ssh/authorized_keys || cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-ssh-keygen -H -F localhost > /dev/null || ssh-keyscan -H localhost >> ~/.ssh/known_hosts
+grep -q $USER@$HOSTNAME ~/.ssh/authorized_keys || cat ~/.ssh/id_rsa.pub >>~/.ssh/authorized_keys
+ssh-keygen -H -F localhost >/dev/null || ssh-keyscan -H localhost >>~/.ssh/known_hosts
 
-which ansible-playbook > /dev/null || echo 'Please reload your profile to have ansible-playbook in your path (probably logout/login or `source ~/.profile`)'
+which ansible-playbook >/dev/null || echo 'Please reload your profile to have ansible-playbook in your path (probably logout/login or `source ~/.profile`)'
